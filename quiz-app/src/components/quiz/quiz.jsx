214454d4081b3e58853ecabe316218ff4efc16d6
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-12 17:36:29 +0530

    added question UI

--- quiz-app/src/components/quiz/quiz.jsx
+++ quiz-app/src/components/quiz/quiz.jsx
@@ -1,8 +1,22 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import './quiz.css'
 
 const quiz = () => {
   return (
-    <div>quiz</div>
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>Which device is required for the Internet Connection?</h2>
+        <ul>
+            <li>ABC</li>
+            <li>LMN</li>
+            <li>XYZ</li>
+            <li>DRZ</li>
+        </ul>
+        <button>Next</button>
+        <div className='index'>1 of 5 questions</div>
+    </div>
   )
 }
 
