32e04d97151a83ae20e4e21ff81dac5f279235f1
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-12 22:20:21 +0530

    Added data into quize app

--- quiz-app/src/components/quiz/quiz.jsx
+++ quiz-app/src/components/quiz/quiz.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import './quiz.css'
+import { data } from '../../assets/data';
 
 const quiz = () => {
+    let [index, setIndex] = useState(1);
+    let [question, setQuestion] = useState(data[index]);
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
-        <h2>Which device is required for the Internet Connection?</h2>
+        <h2>{index + 1}. {question.question}</h2>
         <ul>
-            <li>ABC</li>
-            <li>LMN</li>
-            <li>XYZ</li>
-            <li>DRZ</li>
+            <li>{question.option1}</li>
+            <li>{question.option2}</li>
+            <li>{question.option3}</li>
+            <li>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className='index'>1 of 5 questions</div>
